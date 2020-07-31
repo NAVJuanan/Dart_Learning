41076bdf9d2e0d41e7b38ddc73a8d71654eb626f
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-07-31 21:19:34 +0200

    #Program02: Data Types

--- 01_Dart_Basics/02_Dart_Data_Types.dart
+++ 01_Dart_Basics/02_Dart_Data_Types.dart
@@ -23,8 +23,16 @@ main(List<String> args) {
   print('''String value declared with 'var': ${fullName1}''');
   print('''String value declared with 'String': ${fullName2}''');
 
-  // list
-  var heroeList1 = ['Batman', 'Superman', 'Spiderman'];
+  // boolean
+  var active = true;
+  bool inactive = false;
+
+  print('');
+  print('''Boolean value declared with 'var': ${active}''');
+  print('''Boolean value declared with 'bool': ${inactive}''');
+
+  // list (array in other programming languages)
+  var heroeList1 = ['Batman', 'Superman', 'Spiderman', 'Batman'];
 
   List<String> heroeList2 = new List(2);
   heroeList2[0] = 'Robin';
@@ -34,14 +42,51 @@ main(List<String> args) {
   List<String> heroeList3 = new List();
   heroeList3.add('Batman');
   heroeList3.add('Spiderman');
-  heroeList3.add('Wonder Woman');
+  heroeList3.add('Batman'); // available repeating a member in a list
 
   List<String> heroeList4 = new List();
-  heroeList4..add('Batman')..add('Spiderman')..add('Wonder Woman');
+  heroeList4..add('Batman')..add('Spiderman')..add('Batman');
 
   print('');
   print('''List values declared with 'var': ${heroeList1}''');
   print('''List values declared with fixed 'List': ${heroeList2}''');
   print('''List values declared with dynamic 'List': ${heroeList3}''');
-  print('''List values declared with cascade operator' List': ${heroeList4}''');
+  print('''List values declared with cascade operator 'List': ${heroeList4}''');
+
+  // set (list in other programming languages)
+  var villainSet1 = {'Joker', 'Red Skull', 'Doom', 'Joker'};
+
+  Set<String> villainSet2 = new Set();
+  villainSet2.add('Red Skull');
+  villainSet2.add('Doom');
+  villainSet2.add('Doom'); // not included because it already exists
+
+  Set<String> villainSet3 = new Set();
+  villainSet3..add('Red Skull')..add('Doom')..add('Joker');
+
+  print('');
+  print('''Set values declared with 'var': ${villainSet1}''');
+  print('''Set values declared with 'Set': ${villainSet2}''');
+  print('''Set values declared with cascade operator 'Set': ${villainSet3}''');
+
+  // map (dictionary or object in other programming languages)
+  var powerMap1 = {
+    'name': 'Steve Rogers',
+    'alias': 'Captain America',
+    'age': 25,
+    'country': 'USA',
+    'active': true
+  };
+
+  Map<String, Object> powerMap2 = {
+    'name': 'Natasha',
+    'alias': 'Black Widow',
+    'age': 22,
+    'country': 'Russia',
+    'active': false
+  };
+
+  print('');
+  print('''Map values declared with 'var': ${powerMap1}''');
+  print('''Map values declared with 'Map': ${powerMap2}''');
 }
