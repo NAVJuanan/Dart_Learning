03d0d0f116fb9abf0577bb762b1fd3cd3145089b
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-07-31 18:37:05 +0200

    #Program02: Data Types

---
+++ 01_Dart_Basics/02_Dart_Data_Types.dart
@@ -0,0 +1,25 @@
+main(List<String> args) {
+  // integers
+  var numberInteger1 = 14;
+  int numberInteger2 = 8;
+
+  print('');
+  print('''Integer value declared with 'var': ${numberInteger1}''');
+  print('''Integer value declared with 'int': ${numberInteger2}''');
+
+  // floats
+  var numberDouble1 = 13.2;
+  double numberDouble2 = 1.55;
+
+  print('');
+  print('''Double value declared with 'var': ${numberDouble1}''');
+  print('''Double value declared with 'double': ${numberDouble2}''');
+
+  // strings
+  var fullName1 = 'John Doe';
+  String fullName2 = 'Joe Doe';
+
+  print('');
+  print('''String value declared with 'var': ${fullName1}''');
+  print('''String value declared with 'String': ${fullName2}''');
+}
