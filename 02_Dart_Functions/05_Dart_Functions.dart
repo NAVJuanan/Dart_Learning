ce476c1b62863184cd8f9143cf73edd506a0be23
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-08-03 10:12:16 +0200

    #Program05: Functions

--- 02_Dart_Functions/05_Dart_Functions.dart
+++ 02_Dart_Functions/05_Dart_Functions.dart
@@ -4,30 +4,30 @@ main(List<String> args) {
   stdout.writeln('What is your name?');
   String name = stdin.readLineSync();
 
-  printmessage1(name);
-  print(printmessage2());
-  printmessage3(name);
-  printmessage4(times: 2, lastName: name, firstName: 'Mr.');
+  printMessage1(name);
+  print(printMessage2());
+  printMessage3(name);
+  printMessage4(times: 2, lastName: name, firstName: 'Mr.');
 }
 
 // parameter
-void printmessage1(String message) {
+void printMessage1(String message) {
   stdout.writeln('Hi ${message}, how are you?');
 }
 
 // return value
-String printmessage2() {
+String printMessage2() {
   return 'I am fine thanks, and you?';
 }
 
 // optional parameter
-void printmessage3(String firstName, [String lastName = 'Doe']) {
+void printMessage3(String firstName, [String lastName = 'Doe']) {
   lastName ??= '<Doe>'; // if null -> write a default value
   stdout.writeln('Hi ${firstName} ${lastName}, how are you?');
 }
 
 // parameter by name
-void printmessage4({String firstName, String lastName, int times}) {
+void printMessage4({String firstName, String lastName, int times}) {
   for (var i = 0; i < times; i++) {
     stdout.writeln('Hi ${firstName} ${lastName}, how are you?');
   }
