8cbd35f9fa232ac7b26b4c6e320e96ed684f45dd
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-08-03 10:12:36 +0200

    #Program06: Spread Operator

--- 02_Dart_Functions/06_Dart_Spread.dart
+++ 02_Dart_Functions/06_Dart_Spread.dart
@@ -3,7 +3,7 @@ main(List<String> args) {
   Map<String, dynamic> onePerson = {'name': 'John', 'age': 24};
   print('Map by reference');
   print(onePerson);
-  print(capitalizemap1(onePerson));
+  print(capitalizeMap1(onePerson));
   print(onePerson);
   print('');
 
@@ -11,18 +11,18 @@ main(List<String> args) {
   Map<String, dynamic> otherPerson = {'name': 'Mary', 'age': 26};
   print('Map by value with spread operator');
   print(otherPerson);
-  print(capitalizemap2(otherPerson));
+  print(capitalizeMap2(otherPerson));
   print(otherPerson);
 }
 
 // by reference
-Map<String, dynamic> capitalizemap1(Map<String, dynamic> person) {
+Map<String, dynamic> capitalizeMap1(Map<String, dynamic> person) {
   person['name'] = person['name'].toUpperCase();
   return person;
 }
 
 // by value
-Map<String, dynamic> capitalizemap2(Map<String, dynamic> person) {
+Map<String, dynamic> capitalizeMap2(Map<String, dynamic> person) {
   person = {...person}; // spread operator
   person['name'] = person['name'].toUpperCase();
   return person;
